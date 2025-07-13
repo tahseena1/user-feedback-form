fe4d49926cf6bb49d9a6a2a1d04abbb98253028f
Tahseena Badat <tahseena@usf.edu>
2025-07-13 18:38:12 +0200

    Added eventlistener, and character count

--- cp_1.js
+++ cp_1.js
@@ -1,6 +1,18 @@
-const userInputForm = document.getElementById("user-input-form");
+document.addEventListener("DOMContentLoaded", function () {
+
+const form = document.getElementById("user-input-form");
+const fields = form.querySelectorAll("input, textarea");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const commentBox = document.getElementById("comments");
+const characterCount = document.getElementById("character-count");
 const submitButton = document.getElementById("submit");
 
+commentBox.addEventListener("input", function () {
+    const count = commentBox.value.length;
+    if (count === 1) {
+        characterCount.textContent = `${count} character`;
+    }
+    else {characterCount.textContent = `${count} characters`};
+});
+}
