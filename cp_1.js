d4e1f035aa2319db25c8cce43019a6355160c8bf
Tahseena Badat <tahseena@usf.edu>
2025-07-13 18:19:37 +0200

    Established variables from index.html in cp_1.js

---
+++ cp_1.js
@@ -0,0 +1,6 @@
+const userInputForm = document.getElementById("user-input-form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const commentBox = document.getElementById("comments");
+const submitButton = document.getElementById("submit");
+
